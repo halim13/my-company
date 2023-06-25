a22215199b0c4fa15061d057764e4fc2750e7e88
halim13 <hasanudinhalim@gmail.com>
2023-06-26 00:17:52 +0700

    adjust: our value section

--- src/sections/OurValues.jsx
+++ src/sections/OurValues.jsx
@@ -18,7 +18,7 @@ const item_values = [
 
 function OurValues() {
     return (
-        <div className="container my-24 mx-auto md:px-6">
+        <div className="container my-24 mx-auto md:px-6" id='ourvalues'>
             <section className="mb-32">
                 <h1 className="mb-12 text-3xl font-bold text-center" style={{ color: '#029FE4' }}>Our Core Values</h1>
                 <div className="flex flex-wrap items-center">
@@ -39,9 +39,9 @@ function OurValues() {
                             item_values.map((item, i) => <div key={i} className="mb-12 flex">
                                 <div className="shrink-0">
                                     <div className="rounded-md p-4 shadow-lg bg-[hsl(231,52.6%,20%)] dark:bg-[hsl(231,52.6%,30%)]">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2"
                                             stroke="currentColor" className="h-6 w-6 text-white">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                            <path strokeLinecap="round" strokeLinejoin="round"
                                                 d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z" />
                                         </svg>
                                     </div>
